6233663970230afe2166f15fe1977fd07a6bba31
APARIACURI <pariacuri012@gmail.com>
2026-05-26 23:36:30 -0500

    mi commit proyecto

---
+++ src/main/java/com/utp/bibliotecaENP/BibliotecaEnpApplication.java
@@ -0,0 +1,13 @@
+package com.utp.bibliotecaENP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaEnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaEnpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/bibliotecaENP/BibliotecaEnpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.bibliotecaENP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaEnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
